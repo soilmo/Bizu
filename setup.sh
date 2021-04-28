e11c913782f226dea2e74202d13d9845477414e7
soilmo <60858896+soilmo@users.noreply.github.com>
2021-04-28 11:12:39 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 mkdir -p ~/.streamlit/
 
-echo "[theme]\n\
+echo "\
+[general]\n\
+email = ""\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+[theme]\n\
 primaryColor = ‘#84a3a7’\n\
 backgroundColor = ‘#EFEDE8’\n\
 secondaryBackgroundColor = ‘#fafafa’\n\
 textColor= ‘#424242’\n\
 font = ‘sans serif’\n\
-\n\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
